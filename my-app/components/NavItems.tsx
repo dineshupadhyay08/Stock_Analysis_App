9adbbf7b75bb4c78cd7c45db775f2b03a4f6ec92
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-05-27 23:48:14 +0530

    Fix the UI Error

--- my-app/components/NavItems.tsx
+++ my-app/components/NavItems.tsx
@@ -4,14 +4,16 @@ import { callbackify } from 'util'
 
 const NavItems = () => {
   return (
-    <ul className='flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium'>
-      {NAV_ITEMS.map(callbackfn:({isHmrRefresh,lable})=>(
+    <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+      {NAV_ITEMS.map(({ href, label }) => (
         <li key={href}>
-          <Link href={href} className=''
+          <Link href={href} className="">
+            {label}
+          </Link>
         </li>
       ))}
     </ul>
-  )
+  );
 }
 
 export default NavItems
