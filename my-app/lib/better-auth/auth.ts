94ca7644b245d83ae9a9dd6c9c1497c8eec5d76f
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-06-03 22:43:37 +0530

    Add Databse and APIs

--- my-app/lib/better-auth/auth.ts
+++ my-app/lib/better-auth/auth.ts
@@ -10,8 +10,7 @@ let authInstance: ReturnType<typeof betterAuth> | null = null;
 export const getAuth = async () => {
   if (authInstance) return authInstance;
 
-  const mongoose = await connectToDatabase();
-  const db = mongoose.connection.db;
+  
 
   if (!db) throw new Error("MongoDB connection not found");
 
