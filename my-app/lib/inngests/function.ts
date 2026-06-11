2039f4765fb36a7d659b955af138675ebd407118
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-06-11 20:49:49 +0530

    Add email function in inngest

--- my-app/lib/inngests/function.ts
+++ my-app/lib/inngests/function.ts
@@ -20,60 +20,6 @@ type UserForNewsEmail = {
 // Keep it permissive because the exact structure comes from finnhub action.
 type MarketNewsArticle = Record<string, unknown>;
 
-// export const sendSignUpEmail = inngest.createFunction(
-//   { id: "sign-up-email" },
-//   [{ event: "app/user.created" }],
-// async ({ event, step }) => {
-//     const userProfile = `
-//       - Country: ${event.data.country}
-//       - Investment goals: ${event.data.investmentGoals}
-//       - Risk tolerance: ${event.data.riskTolerance}
-//       - Preferred industry: ${event.data.preferredIndustry}
-//     `;
-
-//     const prompt = PERSONALIZED_WELCOME_EMAIL_PROMPT.replace(
-//       "{{userProfile}}",
-//       userProfile,
-//     );
-
-//     const response = await step.ai.infer("generate-welcome-intro", {
-//       model: step.ai.models.gemini({
-//         model: "gemini-2.5-flash-lite",
-//       }),
-//       body: {
-//         contents: [
-//           {
-//             role: "user",
-//             parts: [{ text: prompt }],
-//           },
-//         ],
-//       },
-//     });
-
-//     await step.run("send-welcome-email", async () => {
-//       const part = response.candidates?.[0]?.content?.parts?.[0];
-
-//       const introText =
-//         (part && "text" in part ? part.text : null) ||
-//         "Thanks for joining Signalist. You now have the tools to track markets and make smarter moves.";
-
-//       const {
-//         data: { email, name },
-//       } = event;
-
-//       return await sendWelcomeEmail({
-//         email,
-//         name,
-//         intro: introText,
-//       });
-//     });
-
-//     return {
-//       success: true,
-//       message: "Welcome email sent successfully",
-//     };
-//   },
-// );
 
 export const sendSignUpEmail = inngest.createFunction(
   {
@@ -165,6 +111,7 @@ export const sendSignUpEmail = inngest.createFunction(
 );
 
 export const sendDailyNewsSummary = inngest.createFunction(
+  
   {
     id: "daily-news-summary",
     triggers: [{ event: "app/send.daily.news" }, { cron: "0 12 * * *" }],
@@ -172,6 +119,7 @@ export const sendDailyNewsSummary = inngest.createFunction(
   async ({ step }) => {
     // Step #1: Get all users for news delivery
     const users = await step.run("get-all-users", getAllUsersForNewsEmail);
+    
 
     if (!users || users.length === 0)
       return { success: false, message: "No users found for news email" };
--- my-app/lib/inngests/route.ts
+++ my-app/lib/inngests/route.ts
@@ -2,7 +2,10 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngests/client";
 import { sendSignUpEmail, sendDailyNewsSummary } from "@/lib/inngests/function";
 
-console.log("INNGEST ROUTE LOADED");
+// console.log("INNGEST ROUTE LOADED");
+console.log("DAILY NEWS FUNCTION LOADED");
+console.log("SIGNUP:", sendSignUpEmail?.opts?.id);
+console.log("DAILY:", sendDailyNewsSummary?.opts?.id);
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
