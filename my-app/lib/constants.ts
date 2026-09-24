791292dd2d015a53149a9853ed84a58eace52336
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-09-24 23:34:40 +0530

    Improve lib and constants

--- my-app/lib/constants.ts
+++ my-app/lib/constants.ts
@@ -2,6 +2,7 @@ export const NAV_ITEMS = [
   { href: "/", label: "Dashboard" },
   { href: "/search", label: "Search" },
   { href: "/watchlist", label: "Watchlist" },
+  { href: "/market-news", label: "Market News" },
 ];
 
 
