4d940cdbe243f0fbc2745cbaa05ca1614f1c7494
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-09-24 23:51:04 +0530

    Improve css and global css

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -5,6 +5,30 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.resolve(__dirname),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static2.finnhub.io',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.cnbcfm.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'data.bloomberglp.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.finnhub.io',
+        pathname: '/logo/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
